b0abea09d974a27d0e2db2557eb0f78b7a64f398
Daniel He <dhe30@stuy.edu>
2024-08-30 00:14:09 -0400

    view less/more functionality added

--- src/components/Scdescriptor.js
+++ src/components/Scdescriptor.js
@@ -1,14 +1,25 @@
 import rightUp from "../assets/icon _arrow right up_.svg";
 import down from "../assets/down-icon-512x294-3o0g6g0d 1.svg";
-function Scdescriptor({title, description, date}) {
+import { useState } from "react";
+function Scdescriptor({title, description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", date}) {
+    const [view, setView] = useState(false);
+    const handleClick = () => {
+        setView(!view);
+    }
     return (
         <div>
             <h1 className="jetbrains-mono-yuh" style={{fontSize:"30px", fontWeight:"bold"}}>
                 <a className="naked" href="https://github.com/">{title || "Super Great Project"} <img src={rightUp}></img></a>
             </h1>
             <h2 className="jetbrains-mono-yuh my-4" style={{fontSize:"18px", fontWeight:"bold"}}>{date || "March 2018"}</h2>
-            <p className="inter-love" style={{fontSize:"16px", lineHeight:"25px"}}> {description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</p>
-            <span className="d-block" style={{fontSize:"16px", fontWeight:"bold", marginTop:"-10px"}}>View more <img src={down}></img></span>
+            <p className="inter-love" style={{fontSize:"16px", lineHeight:"25px"}}> {!view ?  description.substring(0,description.substring(0, 371).lastIndexOf(" "))+"..." : description}</p>
+            <button 
+                className="d-block p-0 unbutton" 
+                style={{fontSize:"16px", fontWeight:"bold", marginTop:"-10px"}}
+                onClick={handleClick}
+            >
+                View {!view? "more": "less"}
+            <img className="ms-1" src={down}></img></button>
         </div>
     )
 }
